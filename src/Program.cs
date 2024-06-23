9409d9d038bf573b14c4568c0c4cf5821c772f6d
ChayoteJarocho <77412126+ChayoteJarocho@users.noreply.github.com>
2024-06-23 13:18:20 -0700

    Add NordVpn checking support.

--- src/Program.cs
+++ src/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Net.Http.Json;
+using VpnChecker;
+
+using HttpClient client = new HttpClient();
+NordVpn? vpn = await client.GetFromJsonAsync<NordVpn>(NordVpn.URL);
+
+if (vpn == null)
+{
+    Console.WriteLine("Could not retrieve the vpn status json.");
+    Environment.Exit(1);
+}
+
+Console.WriteLine(vpn.Status);
+
